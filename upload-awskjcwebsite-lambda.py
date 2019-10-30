10ca8586fccc5bc2e275f9a37c8fe87ef0c28ab3
Ken Cartwright <kennethjcartwright@gmail.com>
2019-10-30 10:57:02 -0400

    Updated lambda

--- upload-awskjcwebsite-lambda.py
+++ upload-awskjcwebsite-lambda.py
@@ -1,4 +1,4 @@
-iimport json
+import json
 import boto3
 import StringIO
 import zipfile
@@ -9,24 +9,43 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:582903906649:KJCWebSite-Deployment-Topic')
 
+    build_location = {
+        "bucketName": 'kjc-deployment-pkgs-bucket',
+        "objectKey": 'KJCWebSite-GitHub-Build.zip'
+    }
+
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                print "Artifact Name:" + str(artifact["name"])
+                if artifact["name"] == "BuildArtifact":
+                    build_location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(build_location)
+
+#        s3 = boto3.resource('s3', config-Config(signature_version='s3v4'))
         s3 = boto3.resource('s3')
 
-        build_bucket = s3.Bucket('kjc-deployment-pkgs-bucket')
         website_bucket = s3.Bucket('www.kennethjcartwright.org')
+        build_bucket = s3.Bucket(build_location["bucketName"])
 
         build_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('KJCWebSite-GitHub-Build.zip', build_zip)
+        build_bucket.download_fileobj(build_location["objectKey"], build_zip)
 
         with zipfile.ZipFile(build_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                print(nm)
+                print "File in Zip:" + nm
                 website_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-
                 website_bucket.Object(nm).Acl().put(ACL='public-read')
 
+        print "Job done."
         topic.publish(Subject="KJCWebSite Deployment Succeeded", Message="KJC WebSite deployed.")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId = job["id"])
 
     except:
         topic.publish(Subject="KJCWebSite Deployment Failed", Message="KJC WebSite failed to deploy.")
