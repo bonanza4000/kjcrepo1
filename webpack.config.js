1e1ce59ba0dfbb09bcc9854a21436f7aee7b07a9
Ken Cartwright <kennethjcartwright@gmail.com>
2019-10-31 12:00:43 -0400

    added webpack and npm

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'kjcwebsite_webpack_bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react']
+        }
+      }
+    }]
+  }
+}
