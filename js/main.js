7f1d8273cdf049368f5ecd02c3f31c92c92dfcbd
Ken Cartwright <kennethjcartwright@gmail.com>
2019-11-03 01:09:24 -0400

    update

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
 
+console.log("main javascript test log");
 ReactDOM.render(<ExampleWork />, document.getElementById('example-work'));
